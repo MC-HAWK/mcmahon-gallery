0d1ce4919d071b348bcc99527eb23501f4e9ebd7
kmcmahon <khawkmcmahon@gmail.com>
2025-06-15 10:31:52 -0400

    Update index.html title and paths; configure Vite for asset management

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mcmahon-gallery/', // Update this to match your repository name
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
 }) 
