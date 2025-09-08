3885b19cbe19f615989f399c6926341184175695
kmcmahon <khawkmcmahon@gmail.com>
2025-09-08 14:57:19 -0400

    Refactor CurrentWorkPaintings component to replace placeholder images with new artwork; enhance image descriptions and titles for better presentation. Update ExperimentalHome component to simplify layout and remove unused carousel functionality, improving performance and user experience. Revise Home component to implement a functional image carousel with navigation controls and auto-advance feature for enhanced interactivity.

--- src/pages/CurrentWorkPaintings.tsx
+++ src/pages/CurrentWorkPaintings.tsx
@@ -12,36 +12,108 @@ const CurrentWorkPaintings = () => {
   const [selectedImage, setSelectedImage] = useState<Image | null>(null);
 
   const images: Image[] = [
-    // {
-    //   id: '1',
-    //   url: 'https://live.staticflickr.com/65535/54605147826_37280e7e3d_b.jpg',
-    //   title: 'Contemporary Landscape I',
-    //   description: 'Oil on canvas \n 48" x 60"'
-    // },
-    // {
-    //   id: '2',
-    //   url: 'https://live.staticflickr.com/65535/54605356478_0a37fb32b5_b.jpg',
-    //   title: 'Modern Abstract',
-    //   description: 'Acrylic on canvas \n 36" x 48"'
-    // },
-    // {
-    //   id: '3',
-    //   url: 'https://live.staticflickr.com/65535/54605147831_df11a67227_b.jpg',
-    //   title: 'Urban Scene',
-    //   description: 'Oil on canvas \n 40" x 50"'
-    // },
-    // {
-    //   id: '4',
-    //   url: 'https://live.staticflickr.com/65535/54605340734_03b826ed7b_b.jpg',
-    //   title: 'Contemporary Still Life',
-    //   description: 'Acrylic on canvas \n 30" x 40"'
-    // },
-    // {
-    //   id: '5',
-    //   url: 'https://live.staticflickr.com/65535/54605147841_6f03a24daf_b.jpg',
-    //   title: 'Modern Portrait',
-    //   description: 'Oil on canvas \n 24" x 30"'
-    // }
+    {
+      id: '1',
+      url: 'https://live.staticflickr.com/65535/54762978071_8a584e36d7_b.jpg',
+      title: 'Masks and Rafters',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '2',
+      url: 'https://live.staticflickr.com/65535/54762978061_8b1a3aef50_b.jpg',
+      title: 'Parenthesis',
+      description: 'Acrylic/Oil on Canvas'
+    },
+    {
+      id: '3',
+      url: 'https://live.staticflickr.com/65535/54762131102_54b65aceb0_b.jpg',
+      title: 'Flaco\'s Purchase',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '4',
+      url: 'https://live.staticflickr.com/65535/54762978086_b776acf372_b.jpg',
+      title: 'Snapper',
+      description: 'Watercolor on Paper \n 12" x 9"'
+    },
+    {
+      id: '5',
+      url: 'https://live.staticflickr.com/65535/54762978091_9eda671c05_b.jpg',
+      title: 'Hanging Masks',
+      description: 'Watercolor on Paper \n 6" x 9"'
+    },
+    {
+      id: '6',
+      url: 'https://live.staticflickr.com/65535/54762978131_5a2083c2e6_b.jpg',
+      title: 'Pocket Pool',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '7',
+      url: 'https://live.staticflickr.com/65535/54762978166_6d0f7e2871_b.jpg',
+      title: 'Blindside',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '8',
+      url: 'https://live.staticflickr.com/65535/54762978186_4f718ace36_b.jpg',
+      title: 'Y and Y-Knot',
+      description: 'Oil/Acrylic on Canvas \n 60" x 40"'
+    },
+    {
+      id: '9',
+      url: 'https://live.staticflickr.com/65535/54763207904_fc1ecc64fb_b.jpg',
+      title: 'Christmas Cracker',
+      description: 'Watercolor/Gouache on Paper \n 28" x 36"'
+    },
+    {
+      id: '10',
+      url: 'https://live.staticflickr.com/65535/54763207944_f61c8692e6_b.jpg',
+      title: 'For R.D.',
+      description: 'Oil on Canvas'
+    },
+    {
+      id: '11',
+      url: 'https://live.staticflickr.com/65535/54763216103_2b4f3ea3ef_b.jpg',
+      title: 'Erin\'s Turn',
+      description: 'Watercolor on Paper \n 6" x 9"'
+    },
+    {
+      id: '12',
+      url: 'https://live.staticflickr.com/65535/54763216173_ea2b2657b3_b.jpg',
+      title: 'Hackberry Drift',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '13',
+      url: 'https://live.staticflickr.com/65535/54763216178_2a505ff153_b.jpg',
+      title: 'Shady Side 2024',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '14',
+      url: 'https://live.staticflickr.com/65535/54763216243_eeb2ef09b9_b.jpg',
+      title: 'Hackberry Winter',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '15',
+      url: 'https://live.staticflickr.com/65535/54763216288_128b27dcfb_b.jpg',
+      title: 'Bones',
+      description: 'Watercolor/Gouache on Paper \n 26" x 38"'
+    },
+    {
+      id: '16',
+      url: 'https://live.staticflickr.com/65535/54763319930_0474045fbd_b.jpg',
+      title: 'Storm Blind',
+      description: 'Gouache/Watercolor on Paper \n 20" x 30"'
+    },
+    {
+      id: '17',
+      url: 'https://live.staticflickr.com/65535/54763319955_a210c78a09_b.jpg',
+      title: 'T with B',
+      description: 'Oil/Acrylic on Canvas \n 24" x 10"'
+    }
   ];
 
   const handleImageClick = (image: Image) => {
